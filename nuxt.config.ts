aecac550c79be9d8722c7747ab3e90c54ced32aa
admin <admin@MacBook-Pro-admin.local>
2023-09-08 05:49:05 +0300

    refactoring

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,10 +19,6 @@ export default defineNuxtConfig({
 })
 
 
-
-
-
-
 // import function to register Swiper custom elements
 // import { register } from 'swiper/element/bundle';
 // // register Swiper custom elements
