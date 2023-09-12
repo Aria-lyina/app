8f038f4feb324e3aae41c28728685939775b6633
Vlad <vladi2505@gmail.com>
2023-09-12 19:28:28 +0300

    Review #2

--- components/VideoBox.vue
+++ components/VideoBox.vue
@@ -1,21 +1,18 @@
 <script setup>
-
-import ButtonPlay from '@/components/ButtonPlay.vue';
-
 defineProps({
     video:Object,
 })
 
-// onMounted(() => 
+// onMounted(() =>
 // {
-//     const btn = 
+//     const btn =
 //     {
 //         diametr:"100px",
 //         color1: "white",
 //         color2: "green"
 //     }
 // })
-const btn = 
+const btn =
     {
         diametr:"100px",
         color1: "red",
@@ -25,9 +22,15 @@ const btn =
 </script>
 
 <template>
-    <div class="container">
-        <div class="container-prevew">
-            <button-play class="bt"/>
+    <div class="video-box-item">
+        <div class="video-box-item-prevew">
+            <div class="bt">
+                <div class="button-play">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>play</title>
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z"/>
+                    </svg>
+                </div>
+            </div>
             <img :src="video.preview" style="width: 100%;"/>
             <!-- <img src="img/apple-iphone-11-green.jpeg" style="width: 100%;"/> -->
             <!-- <img src="img/apple-iphone-11-green.jpeg" style="width: 100%; object-fit: contain;"/> -->
@@ -39,11 +42,20 @@ const btn =
 </template>
 
 <style scoped lang="scss">
-.container
+.video-box-item
 {
     // background-color: blueviolet;
     width: 100%;
     height: 100%;
+
+    &:hover{
+        cursor: pointer;
+
+        .button-play{
+            color: #fff;
+            background: #05a148;
+        }
+    }
 
     &-prevew
     {
@@ -72,11 +84,11 @@ const btn =
 {
     height: 10%;
     width: 90%;
-    font-size: var(--fontsize-light-small-default); 
+    font-size: var(--fontsize-light-small-default);
     color: var(--primary-color-text);
     display: flex;
     align-items:flex-end;
-    align-items:center; 
+    align-items:center;
 }
 
 .description
@@ -88,9 +100,28 @@ const btn =
     font-size: var(--fontsize-text-video);
     display: flex;
     align-items:flex-end;
-    align-items:center; 
+    align-items:center;
     line-height: 1.5;
-    
+
 }
 
+.button-play {
+    background: #fff;
+    color: #05a148;
+    border-radius: 100%;
+    width: 64px;
+    height: 64px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+        width: 18px;
+        height: auto;
+
+        path {
+            fill: currentColor;
+        }
+    }
+}
 </style>
